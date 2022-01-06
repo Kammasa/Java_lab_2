26ed7a0382e304d6670c8692e79a9ac64ee9b90e
kamil <masacz88@gmail.com>
2022-01-06 19:47:50 +0100

    cw.3

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,73 @@ public class Main {
           b) program który określi czy liczba jest podzielna przez 7
          */
 
+        Scanner scan = new Scanner((System.in));
+        System.out.println("Wybierz opcję: ");
+        System.out.println("1 - określenie parzystości");
+        System.out.printf("2 - określenie podzielności przez 7\n");
+        switch (scan.nextInt())
+        {
+            case 1 :
+                int a;
+                System.out.println("Podaj Liczbe całkowitą");
+                a = scan.nextInt();
+                if (a % 2 == 0)
+                {
+                    System.out.println("Liczba jest parzysta.");
+                }
+                else
+                {
+                    System.out.println("Liczba jest nieparzysta.");
+                }
+                break;
+
+            case 2 :
+                int b;
+                System.out.println("Podaj Liczbe całkowitą");
+                b = scan.nextInt();
+                if (b % 7 == 0)
+                {
+                    System.out.printf("Liczba %s jest podzielna przez 7\n",b);
+                }
+                else
+                {
+                    System.out.printf("Liczba %s nie jest podzielna przez 7\n",b);
+                }
+                break;
+        }
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
 
+        String text = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        int[] litery = {0, 0, 0, 0, 0, 0};
+        /* toCharArray() - metoda zamienia String na tablicę char[] */
+        for (char zmiennaChar : text.toCharArray()) {
+            switch (zmiennaChar) {
+                case 'o':
+                    ++litery[0];
+                    break;
+                case 'p':
+                    ++litery[1];
+                    break;
+                case 'x':
+                    ++litery[2];
+                    break;
+                case 'z':
+                    ++litery[3];
+                    break;
+                case 'q':
+                    ++litery[4];
+                    break;
+                case 'n','i','j','m','t','y':
+                    ++litery[5];
+                    break;
+            }
+        }
+        System.out.printf("\nLiczba występuje\no: %d - razy\np: %d - razy\nx: %d - razy\nz: %d - razy\nq: %d - razy\npozostałe litery: %d - razy\n",
+                litery[0], litery[1], litery[2], litery[3], litery[4],litery[5]);
 
     }
 }
