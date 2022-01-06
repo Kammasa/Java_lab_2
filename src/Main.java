cc0c7f4493b2957c3eb2cc424ca835a1924621d1
kamil <masacz88@gmail.com>
2022-01-06 17:34:40 +0100

    cw.2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,13 +14,13 @@ public class Main {
         int i = 1000;
         do
         {
-            if (i % 4 == 0)
-        {
-            System.out.printf("Liczba %s jest podzielna przez liczbe 4\n", i);
+            if (i % 4 == 0 && i > 0)
+            {
+                System.out.printf("Liczba %s jest podzielna przez liczbe 4\n", i);
+            }
         }
-            i--;
-        }
-        while (i >= 0);
+        while (--i >= 0) ;
+
 
         /* zad 2
         a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
@@ -30,45 +32,50 @@ public class Main {
         //a)
         int[] tabInt = {4,7,13,17,24,};
         int i = 0;
-        while (i < tabInt.length)
-        {
-            System.out.println(tabInt[i]);
-            i++;
-        }
+            while (i < tabInt.length)
+            {
+                System.out.println(tabInt[i]);
+                i++;
+            }
 
         //b)
         double[] tabDouble = {4.5,7.8,13.43,17.54,24.87,};
         int i = 0;
-        while (i < tabDouble.length)
-        {
-            System.out.println(tabDouble[i]);
-            i++;
-
+            while (i < tabDouble.length)
+            {
+                System.out.println(tabDouble[i]);
+                i++;
+            }
 
         //c)
-        String[] tabString = new String[]{"Kasia","Misia","Basia","Ela","Mela"};
-        int i = 0;
-        while (i < tabString.length)
-        {
-            System.out.println(tabString[i]);
-            i++;
-        }
+        String[] tabString = new String[]{"Kasia", "Misia", "Basia", "Ela", "Mela"};
+        int t = tabString.length - 1;
+        String y;
 
+            while(t >= 0)
+            {
+                y = tabString[t];
+                System.out.println(y + " ");
+                t--;
+            }
 
         /* zad.3
         a) sprawdzić jak zachowuje się pętla while gdy w jej warunku damy true --> while(true)
         b) a jak gdy damy false --> while(false)
         */
-        {
-            while (true) //Wykonuje się w nieskończoność
-                System.out.println(1);
-        }
-        {
-            while (false) //Przy false wyświetla błąd
-               System.out.println(1);
-        }
+
+        int a = 8;
+            while (a > 0) //Pętla będzie wypisywała 8 w nieskończoność
+            {
+                System.out.println(a);
+            }
+
+            while (a == 7) //Pętla nic nie wykona
+            {
+                System.out.println("wykonano zadanie");
+            }
 
 
+
+        }
     }
-}
-
