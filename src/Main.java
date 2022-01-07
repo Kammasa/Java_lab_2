8f7b8a87d5384dd3f9926f172fbdf72cee72f3b0
kamil <masacz88@gmail.com>
2022-01-07 19:58:40 +0100

    cw.4

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,37 @@ public class Main {
         c) w trzeciej pętli return
         */
 
+        // a)
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i == 7)
+            {
+                break;
+            }
+            System.out.println(i); // wyświetla się tylko do 6
+        }
+        System.out.println();
 
+        // b)
+        for (int j = 1; j <= 10; j++)
+        {
+            if (j == 2)
+            {
+                continue;
+            }
+            System.out.println(j); // Pętla pomija 2
+        }
+        System.out.println();
+
+        // c)
+        for (int k = 1; k < 10; k++)
+        {
+            if (k == 3)
+            {
+                return;
+            }
+            System.out.println(k); // Pętla wyświetla tylko do 2
+
+        }
     }
 }
