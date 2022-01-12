374594eb94478c93dc4030466472ff5be2777911
kamil <masacz88@gmail.com>
2022-01-12 19:10:56 +0100

    cw.5

--- src/Main.java
+++ src/Main.java
@@ -16,30 +16,16 @@ public class Main {
 
         wypisywanieTablicy(uzupełnienieTablicy(tablica1));
 
-        // zad.2
+//zad.2
 //a) stworzyc tablicę typu String[6],
         String[] tablica2 = new String[6];
         wyświetlenieTablicy2(uzupełanianieTablicy2(tablica2));
 
+//zad.3
+        imieNazwiskoWiek("Kamil","Masacz",24);
 
-
-
-        /* zad.3
-        a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
-            wyświetla w konsoli string który ma być waszym imieniem,
-        b) napisać metodę o tej samej nazwie jak w punkcie a) z 2-oma parametrami typu String, która wyświetli
-            wasze imie i nazwisko,
-        c) napisać metodę o tej samej nazwie jak w punkcie a) i b) która przyjmuje wasze imie,
-            nazwisko i wiek (int), a następnie wszystko wyświetli,
-        */
-
-        /* zad.4
-        a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
-            coś takiego:                                    zmiennaA = metoda(1, 2); <-- wywołanie metody
-        b) napisać metodę (nazwa ta sama co w punkcie a) która przyjmuje 3 parametry typu int i zwraca ich sumę,
-            którą przypiszemy do zmiennej, coś takiego:     zmiennaB = metoda(7, 8, 9); <-- wywołanie metody
-        c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
-        */
+//zad.4
+        suma2metod();
 
 
     }
@@ -62,7 +48,7 @@ public class Main {
 
     static String[] uzupełanianieTablicy2(String[] tablica2) {
         Scanner scaner = new Scanner(System.in);
-        System.out.println("Wpisz " + tablica2.length + "słów ");
+        System.out.println("Wpisz " + tablica2.length + " słów");
         for (int j = 0; j < tablica2.length; j++) {
             tablica2[j] = scaner.nextLine();
         }
@@ -71,9 +57,59 @@ public class Main {
 // Zad 2.c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
 
     static String[] wyświetlenieTablicy2(String[] tablica2) {
-        for (int j = 0; j < tablica2.length - 1; j--) {
-            System.out.println(tablica2[j] + " ");
+        int zmienna = tablica2.length - 1;
+        for (int j = 0; j < tablica2.length; j++) {
+            System.out.println(tablica2[zmienna - j] + " ");
         }
         return tablica2;
     }
+
+// Zad.3.a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
+   //wyświetla w konsoli string który ma być waszym imieniem,
+
+    static void imie(String imie){
+        System.out.println(imie + " ");
+    }
+//Zad.3.b) napisać metodę o tej samej nazwie jak w punkcie a) z 2-oma parametrami typu String, która wyświetli
+    //wasze imie i nazwisko,
+
+    static void imieNazwisko(String imie, String nazwisko){
+        System.out.println(imie + " ");
+        System.out.println(nazwisko + " ");
+    }
+
+//Zad.3.c) napisać metodę o tej samej nazwie jak w punkcie a) i b) która przyjmuje wasze imie,
+    //nazwisko i wiek (int), a następnie wszystko wyświetli,
+
+    static void imieNazwiskoWiek(String imie, String nazwisko, int wiek){
+        System.out.println(imie + " ");
+        System.out.println(nazwisko + " ");
+        System.out.println(wiek + " ");
+    }
+//Zad.4.a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
+
+    static int suma2zmiennych(int a, int b){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj a i b");
+        a = scan.nextInt();
+        b = scan.nextInt();
+        return a + b;
+    }
+//Zad.4.b) napisać metodę (nazwa ta sama co w punkcie a) która przyjmuje 3 parametry typu int i zwraca ich sumę,
+    //którą przypiszemy do zmiennej,
+
+    static int suma3zmiennych(int a, int b, int c){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj c");
+        c = scan.nextInt();
+        return a + b + c;
+    }
+//Zad.4.c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
+
+    static void suma2metod() {
+        System.out.println("Wynik " + (suma2zmiennych(0,0) + suma3zmiennych(0,0,0)));
+    }
+
 }
+
+
