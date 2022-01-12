7d7ffd8311a15c3d3a4ebfc1712da62992c308f3
kamil <masacz88@gmail.com>
2022-01-12 02:24:40 +0100

    cw.5

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,28 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         /* ćwiczenie 5 Metody */
 
-        /* zad.1
-        a) stworzyć tablicę typu int[20],
-        b) napisać metodę która będzie uzupełniała tablicę wartościami od 40 do 21,
-        c) napisać metodę, która wypisze otrzymane wartości,
-        d) wstawić wywołanie metody do metody main
-        */
+// zad.1
+//a) stworzyć tablicę typu int[20],
+        int[] tablica1 = new int[20];
 
-        /* zad.2
-        a) stworzyc tablicę typu String[6],
-        b) napisać metodę która uzupełni tablicę wartościami podawanymi z klawiatury
-            (użyć do tego pętli i klasy Scanner),
-        c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
-        */
+//d) wstawić wywołanie metody do metody main
+
+        wypisywanieTablicy(uzupełnienieTablicy(tablica1));
+
+        // zad.2
+//a) stworzyc tablicę typu String[6],
+        String[] tablica2 = new String[6];
+        wyświetlenieTablicy2(uzupełanianieTablicy2(tablica2));
+
+
+
 
         /* zad.3
         a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
@@ -39,5 +43,37 @@ public class Main {
 
 
     }
+// Zad 1.b) napisać metodę która będzie uzupełniała tablicę wartościami od 40 do 21,
 
+    static int[] uzupełnienieTablicy(int[] tablica1) {
+        for (int i = 0; i < tablica1.length; i++) {
+            tablica1[i] = 40 - i;
+        }
+        return tablica1;
+    }
+// Zad 1.c) napisać metodę, która wypisze otrzymane wartości,
+
+    static void wypisywanieTablicy(int[] tablica1) {
+        for (int i = 0; i < tablica1.length; i++) {
+            System.out.println(tablica1[i] + " ");
+        }
+    }
+// Zad 2.b) napisać metodę która uzupełni tablicę wartościami podawanymi z klawiatury  (użyć do tego pętli i klasy Scanner),
+
+    static String[] uzupełanianieTablicy2(String[] tablica2) {
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Wpisz " + tablica2.length + "słów ");
+        for (int j = 0; j < tablica2.length; j++) {
+            tablica2[j] = scaner.nextLine();
+        }
+        return tablica2;
+    }
+// Zad 2.c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
+
+    static String[] wyświetlenieTablicy2(String[] tablica2) {
+        for (int j = 0; j < tablica2.length - 1; j--) {
+            System.out.println(tablica2[j] + " ");
+        }
+        return tablica2;
+    }
 }
